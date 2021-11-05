00f7f683e6bf54e2e2fdf3a692db2b66cf82d706
ChrisJRamirez <chrisjamesramirez@gmail.com>
2021-11-05 13:19:57 -0400

    taking a break

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -2,11 +2,12 @@ import axios from 'axios';
 
 export const axiosWithAuth = ()=> {
  const token = localStorage.getItem("token");
+ console.log(token)
 
   return axios.create({
     baseURL: "http://localhost:5000/api",
     headers: {
-      authorization: token}
+      authorization: token }
   }) 
 }
 
